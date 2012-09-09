b7f74d46b7e808d3ecdb896abf99120ef7eb9c0b
Sean McClenaghan <cyberkitsune09@gmail.com>
2012-09-09 16:37:33 -0700

    Add source file :)

---
+++ src/main/java/net/lethargiclion/PluginMain.java
@@ -0,0 +1,21 @@
+package net.lethargiclion;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PluginMain extends JavaPlugin {
+	
+	public Logger mcLog = Logger.getLogger("Minecraft");
+	
+	@Override
+	public void onEnable() {
+		mcLog.info("[MailBox] Mailbox version "+getDescription().getVersion()+" enabled!");
+	}
+	
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+	}
+
+}
